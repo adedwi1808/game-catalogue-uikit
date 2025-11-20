c24a2366a478561cc1e5df044ef6a5f3aa73d3c3
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-11-20 09:42:00 +0700

    Initial Commit

---
+++ game-catalogue-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  game-catalogue-uikit
+//
+//  Created by Ade Dwi Prayitno on 20/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
